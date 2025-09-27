2951a3cda5d457324578ab67c392a5653c9bd350
stellarinrin <stellarinrin.922@gmail.com>
2025-09-27 14:58:27 -0500

    Clean up combo box code

--- dinosaur_fighting_card_game/scripts/combo_box.gd
+++ dinosaur_fighting_card_game/scripts/combo_box.gd
@@ -1,6 +1,6 @@
 extends HBoxContainer
 
-@onready var game = $"../../.."
+@onready var game = get_tree().root
 var combo: Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,13 +12,10 @@ func _process(delta: float) -> void:
 	if (get_child_count(true) > 0):
 		get_child(0)._set_rotation(0)
 	queue_sort()
-	
-
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var card_node = area.get_parent()
 	card_node.reparent(self)
-	#add_child(card_node)
 	
 func _parse_cards() -> void:
 	combo = get_children()
@@ -28,6 +25,5 @@ func _parse_cards() -> void:
 func _on_reset_cards_button_down() -> void:
 	var hand = get_children()
 	for card in hand:
-		#card.get_node("Area2D").monitorable = false
 		card.reparent(game)
 		card.position = Vector2(0,0)
