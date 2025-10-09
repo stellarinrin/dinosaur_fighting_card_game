9e01012503a531b440995c222441e92fc62bcbb1
stellarinrin <stellarinrin.922@gmail.com>
2025-10-08 21:15:11 -0500

    Update UI

--- dinosaur_fighting_card_game/scenes/menu_button.gd
+++ dinosaur_fighting_card_game/scenes/menu_button.gd
@@ -1,21 +1,35 @@
 extends Button
 
 @export var animation_start_time: float
+@export var menu: CanvasLayer
+@export var analogous_button: Button
+@export var is_submenu_button: bool
+
 func _ready() -> void:
-	%ButtonAnimation.play("Idle")
+	$ButtonName.text = text
+	%ButtonAnimation.play("idle_front")
+	%ButtonAnimation.seek(animation_start_time,true)
+	%ButtonAnimation.play("idle_back")
 	%ButtonAnimation.seek(animation_start_time,true)
 	%ButtonAnimation.speed_scale = 0.25
-	
-
 
 func _on_mouse_entered() -> void:
 	%ButtonAnimation.speed_scale = 1
 	
-
 func _on_mouse_exited() -> void:
 	%ButtonAnimation.speed_scale = 0.25
-
-func _on_button_down() -> void:
-	%ButtonAnimation.speed_scale = 0.25
-	$TopLayer.z_index = 5
-	%ButtonAnimation.play("menu_popup")
+	
+func _on_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		%ButtonAnimation.speed_scale = 0.25
+		%ButtonAnimation.play("menu_popup")
+		await get_tree().create_timer(1).timeout
+		%ButtonAnimation.play("idle_back")
+		menu.visible = true
+	if not toggled_on:
+		#menu.visible = false
+		%ButtonAnimation.play("menu_collapse")
+		await get_tree().create_timer(1).timeout
+		%ButtonAnimation.play("idle_back")
+		await get_tree().create_timer(.2).timeout
+		%ButtonAnimation.play("idle_front")
