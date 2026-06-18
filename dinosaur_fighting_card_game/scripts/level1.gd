d07b3cd45c637139f8bcb9c6f07fcc9d12b9d5d1
stellarinrin <stellarinrin.922@gmail.com>
2026-06-18 12:14:36 -0500

    Implement simultaneous action
    Speed has yet to be implemented

--- dinosaur_fighting_card_game/scripts/level1.gd
+++ dinosaur_fighting_card_game/scripts/level1.gd
@@ -36,61 +36,61 @@ func _process(delta: float) -> void:
 # Allow player 1 inputs
 # On submit, hide player 1 inputs, allow player 2 inputs
 # On submit, hide player 2 inputs
-func _on_play_hand_pressed() -> void:
-	#maybe put this part in its own function when programming turn cycle
-	var player : Character = player_1
-	var opponent : Character = player_2
-	
-	combo = controller_1.container.get_children()
-	for move in combo:
-		print(combo)
-		match move.move_type:
-			Global.MoveType.MOVEMENT:
-				character_move(move, player, opponent)
-			Global.MoveType.ATTACK:
-				character_attack(move, player, opponent)
-			Global.MoveType.BLOCK:
-				character_block(move, player, opponent)
-			Global.MoveType.THROW:
-				character_throw(move, player, opponent)
-		await get_tree().create_timer(1).timeout
-	combo.clear()
-	
-	
-func character_move(move: Card, player: Character, opponent: Character) -> void:
-	if player.attributes.grid_position.y == 0 or true: # only if the character is grounded
-		player.attributes.grid_position.x += move.horizontal_distance
-		player.attributes.grid_position.y += move.vertical_distance
-	
-func character_attack(move: Card, player: Character, opponent: Character) -> void:
-	var hitbox : Vector2 = player.attributes.grid_position + move.hitbox
-	# Characters will auto-turn to hit their move
-	if (opponent.attributes.grid_position.x > player.attributes.grid_position.x \
-			and opponent.attributes.grid_position.x <= \
-			player.attributes.grid_position.x + move.hitbox.x) or \
-			(opponent.attributes.grid_position.x < player.attributes.grid_position.x \
-			and opponent.attributes.grid_position.x >= \
-			player.attributes.grid_position.x - move.hitbox.x):
-		# If the opponent is at the same height as the player or if they are *standing* at eye 
-		#	level with the player's bottom half, allow the hit
-		if opponent.attributes.grid_position.y == player.attributes.grid_position.y \
-				or (opponent.attributes.grid_position.y + 1 == player.attributes.grid_position.y \
-				and not opponent.attributes.is_crouching):
-			opponent.attributes.h_p -= 10.0
-		# If the opponent is higher than the player, check if the move points up
-		elif (opponent.attributes.grid_position.y >= player.attributes.grid_position.y) \
-				and (move.hitbox.y > 0) and (opponent.attributes.grid_position.y <= \
-				player.attributes.grid_position.y + move.hitbox.y):
-			opponent.attributes.h_p -= 10.0
-		# If the opponent is lower than the player, check if the move points down
-		elif (opponent.attributes.grid_position.y <= player.attributes.grid_position.y) \
-				and (move.hitbox.y < 0) and (opponent.attributes.grid_position.y >= \
-				player.attributes.grid_position.y + move.hitbox.y):
-			opponent.attributes.h_p -= 10.0
-	
-
-func character_block(move: Card, player: Character, opponent: Character) -> void:
-	pass
-
-func character_throw(move: Card, player: Character, opponent: Character) -> void:
-	pass
+#func _on_play_hand_pressed() -> void:
+	# #maybe put this part in its own function when programming turn cycle
+	#var player : Character = player_1
+	#var opponent : Character = player_2
+	#
+	#combo = controller_1.container.get_children()
+	#for move in combo:
+		#print(combo)
+		#match move.move_type:
+			#Global.MoveType.MOVEMENT:
+				#character_move(move, player, opponent)
+			#Global.MoveType.ATTACK:
+				#character_attack(move, player, opponent)
+			#Global.MoveType.BLOCK:
+				#character_block(move, player, opponent)
+			#Global.MoveType.THROW:
+				#character_throw(move, player, opponent)
+		#await get_tree().create_timer(1).timeout
+	#combo.clear()
+	#
+	#
+#func character_move(move: Card, player: Character, opponent: Character) -> void:
+	#if player.attributes.grid_position.y == 0 or true: # only if the character is grounded
+		#player.attributes.grid_position.x += move.horizontal_distance
+		#player.attributes.grid_position.y += move.vertical_distance
+	#
+#func character_attack(move: Card, player: Character, opponent: Character) -> void:
+	#var hitbox : Vector2 = player.attributes.grid_position + move.hitbox
+	## Characters will auto-turn to hit their move
+	#if (opponent.attributes.grid_position.x > player.attributes.grid_position.x \
+			#and opponent.attributes.grid_position.x <= \
+			#player.attributes.grid_position.x + move.hitbox.x) or \
+			#(opponent.attributes.grid_position.x < player.attributes.grid_position.x \
+			#and opponent.attributes.grid_position.x >= \
+			#player.attributes.grid_position.x - move.hitbox.x):
+		## If the opponent is at the same height as the player or if they are *standing* at eye 
+		##	level with the player's bottom half, allow the hit
+		#if opponent.attributes.grid_position.y == player.attributes.grid_position.y \
+				#or (opponent.attributes.grid_position.y + 1 == player.attributes.grid_position.y \
+				#and not opponent.attributes.is_crouching):
+			#opponent.attributes.h_p -= 10.0
+		## If the opponent is higher than the player, check if the move points up
+		#elif (opponent.attributes.grid_position.y >= player.attributes.grid_position.y) \
+				#and (move.hitbox.y > 0) and (opponent.attributes.grid_position.y <= \
+				#player.attributes.grid_position.y + move.hitbox.y):
+			#opponent.attributes.h_p -= 10.0
+		## If the opponent is lower than the player, check if the move points down
+		#elif (opponent.attributes.grid_position.y <= player.attributes.grid_position.y) \
+				#and (move.hitbox.y < 0) and (opponent.attributes.grid_position.y >= \
+				#player.attributes.grid_position.y + move.hitbox.y):
+			#opponent.attributes.h_p -= 10.0
+	#
+#
+#func character_block(move: Card, player: Character, opponent: Character) -> void:
+	#pass
+#
+#func character_throw(move: Card, player: Character, opponent: Character) -> void:
+	#pass
--- dinosaur_fighting_card_game/scripts/player_controller.gd
+++ dinosaur_fighting_card_game/scripts/player_controller.gd
@@ -2,8 +2,11 @@ class_name PlayerController
 extends Control
 
 var card 
+var combo
 @onready var container = $HBoxContainer
 @export var combo_length_limit : int = 5
+@export var player : Character
+@export var opponent : Character
 var combo_length : int = 0
 
 func _process(_delta: float) -> void:
@@ -21,3 +24,58 @@ func _on_combo_box_entered(area: Area2D) -> void:
 	if combo_length >= combo_length_limit:
 		return
 	card.reparent(container)
+	
+# check startup speed to see which action goes first
+func _on_play_hand_pressed() -> void:
+	combo = container.get_children()
+	for move in combo:
+		print(combo)
+		match move.move_type:
+			Global.MoveType.MOVEMENT:
+				character_move(move, player, opponent)
+			Global.MoveType.ATTACK:
+				character_attack(move, player, opponent)
+			Global.MoveType.BLOCK:
+				character_block(move, player, opponent)
+			Global.MoveType.THROW:
+				character_throw(move, player, opponent)
+		await get_tree().create_timer(1).timeout
+	combo.clear()
+
+func character_move(move: Card, player: Character, opponent: Character) -> void:
+	if player.attributes.grid_position.y == 0 or true: # only if the character is grounded
+		player.attributes.grid_position.x += move.horizontal_distance
+		player.attributes.grid_position.y += move.vertical_distance
+	
+func character_attack(move: Card, player: Character, opponent: Character) -> void:
+	var hitbox : Vector2 = player.attributes.grid_position + move.hitbox
+	# Characters will auto-turn to hit their move
+	if (opponent.attributes.grid_position.x > player.attributes.grid_position.x \
+			and opponent.attributes.grid_position.x <= \
+			player.attributes.grid_position.x + move.hitbox.x) or \
+			(opponent.attributes.grid_position.x < player.attributes.grid_position.x \
+			and opponent.attributes.grid_position.x >= \
+			player.attributes.grid_position.x - move.hitbox.x):
+		# If the opponent is at the same height as the player or if they are *standing* at eye 
+		#	level with the player's bottom half, allow the hit
+		if opponent.attributes.grid_position.y == player.attributes.grid_position.y \
+				or (opponent.attributes.grid_position.y + 1 == player.attributes.grid_position.y \
+				and not opponent.attributes.is_crouching):
+			opponent.attributes.h_p -= 10.0
+		# If the opponent is higher than the player, check if the move points up
+		elif (opponent.attributes.grid_position.y >= player.attributes.grid_position.y) \
+				and (move.hitbox.y > 0) and (opponent.attributes.grid_position.y <= \
+				player.attributes.grid_position.y + move.hitbox.y):
+			opponent.attributes.h_p -= 10.0
+		# If the opponent is lower than the player, check if the move points down
+		elif (opponent.attributes.grid_position.y <= player.attributes.grid_position.y) \
+				and (move.hitbox.y < 0) and (opponent.attributes.grid_position.y >= \
+				player.attributes.grid_position.y + move.hitbox.y):
+			opponent.attributes.h_p -= 10.0
+	
+
+func character_block(move: Card, player: Character, opponent: Character) -> void:
+	pass
+
+func character_throw(move: Card, player: Character, opponent: Character) -> void:
+	pass
